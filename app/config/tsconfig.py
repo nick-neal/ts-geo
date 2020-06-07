4223f04abc68fe13efa8dd31e6672738b65a405d
Tennex <tennex@Tennexs-MacBook.local>
2020-06-07 13:54:52 -0600

    updated error handling

--- app/config/tsconfig.py
+++ app/config/tsconfig.py
@@ -9,5 +9,7 @@ if 'APP_ENV' in os.environ:
 else:
     APP_ENV = "DEV" # DEV, TEST, PROD
 APP_HOST = '0.0.0.0'
-APP_PORT = 4080
+APP_PORT = 4081
 APP_DEBUG = True
+HTTP_CONNECT_TIMEOUT = 1 # set how long before you should giveup on an http connection.
+HTTP_READ_TIMEOUT = 3 # set how long before you should give up on reading a response from an http connection.
--- app/route.py
+++ app/route.py
@@ -1,10 +1,12 @@
-from flask import request
+from flask import request, abort, make_response, jsonify
 import logging
 import requests
-from requests.exceptions import HTTPError
+from requests.exceptions import HTTPError, ConnectTimeout, ReadTimeout, SSLError
 import math
 from config.tsurls import GOOGLE_URL
 from config.tskeys import G_DIRECTIONS_API_KEY
+from config.tsconfig import HTTP_CONNECT_TIMEOUT, HTTP_READ_TIMEOUT
+import time
 
 # initialize logger for app
 logger = logging.getLogger(__name__)
@@ -37,19 +39,32 @@ def callDirectionsAPI(start_addr,end_addr,trans_id):
         # find way to restrict US address lookup.
         guri = f"""/maps/api/directions/json?origin={start_addr}&destination={end_addr}&key={G_DIRECTIONS_API_KEY}"""
         full_url = f'{GOOGLE_URL}{guri}'
-        logger.info(f'Calling /maps/api/directions API [trans_id: {trans_id}]')
+        service = "/maps/api/directions"
+        logger.info(f'Calling {service} API [trans_id: {trans_id}]')
         response = ''
 
         try:
-            response = requests.get(full_url)
-
-            response.raise_for_status()
+            response = requests.get(url=full_url, timeout=(HTTP_CONNECT_TIMEOUT, HTTP_READ_TIMEOUT))
         except HTTPError as http_err:
-            logger.error(f'HTTP error occured for [trans_id: {trans_id}]: {http_err}')
-            message['status'] = "HTTP_ERROR"
+            logger.error(f'HTTP error occured on {service} [trans_id: {trans_id}]: {http_err}')
+            eres = jsonify(status="ERROR", error_code="HTTP_ERROR", error_message="There was an HTTP_ERROR on the server side.")
+            abort(make_response(eres,500))
+        except SSLError as ssl_err:
+            logger.error(f'SSL error occured on {service} [trans_id: {trans_id}]: {ssl_err}')
+            eres = jsonify(status="ERROR", error_code="SSL_ERROR", error_message="There was an SSL_ERROR on the server side.")
+            abort(make_response(eres,500))
+        except ConnectTimeout as ct:
+            logger.error(f'Connection Timeout occured on {service} [trans_id: {trans_id}]: {ct}')
+            eres = jsonify(status="ERROR", error_code="HTTP_CONNECT_TIMEOUT", error_message="The server was unable to make an HTTP connection.")
+            abort(make_response(eres,500))
+        except ReadTimeout as rt:
+            logger.error(f'Read Timeout occured on {service} [trans_id: {trans_id}]: {rt}')
+            eres = jsonify(status="ERROR", error_code="HTTP_READ_TIMEOUT", error_message="The server took too long to respond.")
+            abort(make_response(eres,500))
         except Exception as err:
-            logger.error(f'Error occured for [trans_id: {trans_id}]: {err}')
-            message['status'] = "UNKNOWN_ERROR"
+            logger.error(f'Error occured on {service} [trans_id: {trans_id}]: {err}')
+            eres = jsonify(status="ERROR", error_code="UNKNOWN_ERROR", error_message="An unknown error occured on the server side.")
+            abort(make_response(eres,500))
         else :
             if response.status_code == 200:
                 jres = response.json()
@@ -143,43 +158,3 @@ def analyzeGeoCoordinates(geo_list,distance,duration,trans_id):
         i += 1
 
     return new_geo_list
-
-'''
-def analyzeGeoCoordinates(geo_list,distance,duration,trans_id):
-    # used to analyze distance between coordinates
-    # distances is in meters
-    # duration is in seconds
-
-    hours = round(duration/3600) # rounds to the nearest hour.
-    miles = round(distance/1609.344)
-
-    geo_list_offset = round(len(geo_list)/hours)
-    mile_avg = round(miles/hours)
-    print(len(geo_list))
-
-    new_geo_list = []
-    new_geo_list.append(geo_list[0])
-    i = 0
-
-    while i < len(geo_list):
-        glo = i+geo_list_offset
-
-        if glo >= len(geo_list):
-            new_geo_list.append(geo_list[-1])
-            break
-
-        while True:
-            if glo >= len(geo_list):
-                new_geo_list.append(geo_list[-1])
-                i=len(geo_list)
-                break
-            if round(calculateDistance(geo_list[i],geo_list[glo])) >= mile_avg:
-                i = glo
-                new_geo_list.append(geo_list[glo])
-                break
-            else:
-                glo += 1
-                i += 1
-
-    return new_geo_list
-'''
